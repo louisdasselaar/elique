a31d5163745e609a48b1cfcea71d8350a8b80069
louisdasselaar <l.dasselaar02@gmail.com>
2024-04-02 22:31:44 +0200

    Quote Carousel met autoscoll Toegevoed!

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ toggleBtn.onclick = function(){
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
 }
+
+//Quote Carousel
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('#slider input[type="radio"]');
+    let currentSlide = 0;
+
+    setInterval(function() {
+        // Verwijder de 'checked' status van de huidige slide
+        slides[currentSlide].checked = false;
+        
+        // Ga naar de volgende slide, of ga terug naar de eerste slide als we aan het einde zijn
+        currentSlide = (currentSlide + 1) % slides.length;
+        
+        // Stel de 'checked' status in op de nieuwe huidige slide
+        slides[currentSlide].checked = true;
+    }, 3000); // Verander elke 3000 milliseconden (3 seconden) van slide
+});

1cf92dfeb7a2d9f3cefe12203222ddea184f84a2
louisdasselaar <131251905+louisdasselaar@users.noreply.github.com>
2024-04-05 14:45:57 +0200

    Opzet en navbar done

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ toggleBtn.onclick = function(){
         : 'fa-solid fa-bars'
 }
 
+//Navbar scroll
+document.addEventListener("DOMContentLoaded", function() {
+    // Zoek de knoppen en voeg een click event listener toe aan elk
+    document.querySelectorAll('.navbar a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault(); // Voorkom standaardgedrag van de link
+        
+        // Haal de doel-ID op uit de href-attribuut van de link
+        const targetId = this.getAttribute('href').substring(1);
+        
+        // Zoek het doelelement op basis van de ID
+        const targetElement = document.getElementById(targetId);
+        
+        // Scroll naar het doelelement met een soepele animatie
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      });
+    });
+});
+
 //Quote Carousel
 document.addEventListener('DOMContentLoaded', function() {
     const slides = document.querySelectorAll('#slider input[type="radio"]');
@@ -26,5 +45,6 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Stel de 'checked' status in op de nieuwe huidige slide
         slides[currentSlide].checked = true;
-    }, 10000); // Verander elke 3000 milliseconden (3 seconden) van slide
+    }, 8000); // Verander elke 3000 milliseconden (3 seconden) van slide
 });
+

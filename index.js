852a6515cccd858b88ede56064745a97be59929a
louisdasselaar <131251905+louisdasselaar@users.noreply.github.com>
2024-04-07 13:47:58 +0200

    Footer color fixed and js code cleaned up

--- index.js
+++ index.js
@@ -14,42 +14,40 @@ toggleBtn.onclick = function(){
 
 //Navbar scroll
 document.addEventListener("DOMContentLoaded", function() {
-    // Zoek de knoppen en voeg een click event listener toe aan elk
     document.querySelectorAll('.navbar a').forEach(anchor => {
       anchor.addEventListener('click', function(e) {
-        e.preventDefault(); // Voorkom standaardgedrag van de link
+        e.preventDefault();
         
-        // Haal de doel-ID op uit de href-attribuut van de link
         const targetId = this.getAttribute('href').substring(1);
         
-        // Zoek het doelelement op basis van de ID
         const targetElement = document.getElementById(targetId);
         
-        // Scroll naar het doelelement met een soepele animatie
         targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
       });
     });
 });
 
-//Quote Carousel
+//Testimonial Carousel
 document.addEventListener('DOMContentLoaded', function() {
     const slides = document.querySelectorAll('#slider input[type="radio"]');
     let currentSlide = 0;
 
     setInterval(function() {
-        // Verwijder de 'checked' status van de huidige slide
         slides[currentSlide].checked = false;
-        
-        // Ga naar de volgende slide, of ga terug naar de eerste slide als we aan het einde zijn
         currentSlide = (currentSlide + 1) % slides.length;
-        
-        // Stel de 'checked' status in op de nieuwe huidige slide
         slides[currentSlide].checked = true;
-    }, 8000); // Verander elke 3000 milliseconden (3 seconden) van slide
+    }, 8000); 
 });
 
-// Email sturen via contact us
+// Contact Us
 function openEmailPopup() {
-    // Open de e-mailtoepassing met het specifieke e-mailadres
-    window.location.href = "mailto:l.dasselaar02@gmail.com";
+    if (navigator.share) {
+      navigator.share({
+        url: 'mailto:info@elique-events.com'
+      })
+      .then(() => console.log('E-mail gedeeld'))
+      .catch((error) => console.error('Fout bij delen van e-mail', error));
+    } else {
+      window.location.href = "mailto:l.dasselaar02@gmail.com";
+    }
   }
